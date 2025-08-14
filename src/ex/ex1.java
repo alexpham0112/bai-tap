9072d68fb46b8fc4a0ce6475b460013b97b50151
alexpham0112 <alexpham01122006@gmail.com>
2025-08-14 17:46:10 +0700

    ex1

---
+++ src/ex/ex1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex;
+
+/**
+ *
+ * @author ThinkPad
+ */
+public class ex1 {
+    public static void main (String[] args)
+    {
+    int x=1+2;
+    double x2=3.5+8;
+    double x3= 1.0/2;
+    double x4=4.5+7;
+    int x5=4*5+1;
+    int x6=20%3+2-7;
+    System.out.println("1 + 2 = " + x );
+    System.out.println("3.5 + 8 = " + x2 );
+    System.out.println("1 / 2 = " + x3 );
+    System.out.println("4.5 + 7 = " + x4 );
+    System.out.println("4 * 5 + 1 = " + x5 );
+    System.out.println("20 % 3 + 2 - 7 = " + x6 );
+    }
+}
